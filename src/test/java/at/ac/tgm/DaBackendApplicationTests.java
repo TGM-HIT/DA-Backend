c8ea1f89d091001a075c7fdf6c4e26a0f435d032
Michael Pointner <mpointner@tgm.ac.at>
2024-09-18 22:57:46 +0200

    Create empty Spring Boot Project with a Hello World Controller

---
+++ src/test/java/at/ac/tgm/DaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.tgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaBackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
