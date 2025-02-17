8731c19cdbcf15c66e9a151b4103cb954109d575
Michael Pointner <mpointner@tgm.ac.at>
2025-02-18 00:40:31 +0100

    Add Admin role

--- server/src/main/java/at/ac/tgm/controller/AuthenticationController.java
+++ server/src/main/java/at/ac/tgm/controller/AuthenticationController.java
@@ -13,19 +13,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -37,6 +42,9 @@ public class AuthenticationController implements AuthenticationApi {
     
     @Autowired
     private GrantedAuthoritiesMapper grantedAuthoritiesMapper;
+    
+    @Autowired
+    private UserDetailsContextMapper userDetailsContextMapper;
     
     @Autowired
     private Environment env;
@@ -70,8 +78,16 @@ public class AuthenticationController implements AuthenticationApi {
             if (!env.matchesProfiles("dev")) {
                 throw new IllegalArgumentException("Simulate just allowed with profile dev");
             }
-            List<SimpleGrantedAuthority> authorities = user.getMemberOf().stream().map((memberOf) -> new SimpleGrantedAuthority(Util.getCnFromName(memberOf))).toList();
-            TestingAuthenticationToken authenticationToken = new TestingAuthenticationToken(user.getSAMAccountName(), null, grantedAuthoritiesMapper.mapAuthorities(authorities));
+            List<SimpleGrantedAuthority> authoritiesWithGroups = user.getMemberOf().stream().map((memberOf) -> new SimpleGrantedAuthority(Util.getCnFromName(memberOf))).toList();
+            
+            // Add Admin Role if applicable
+            DirContextAdapter dirContextAdapter = new DirContextAdapter(user.getId());
+            UserDetails userDetails = userDetailsContextMapper.mapUserFromContext(dirContextAdapter, user.getSAMAccountName(), authoritiesWithGroups);
+            
+            // Add Teacher or Student Role if applicable
+            Collection<? extends GrantedAuthority> authoritiesWithGroupsAndRoles = grantedAuthoritiesMapper.mapAuthorities(userDetails.getAuthorities());
+            
+            TestingAuthenticationToken authenticationToken = new TestingAuthenticationToken(user.getSAMAccountName(), null, authoritiesWithGroupsAndRoles);
             authenticationToken.setDetails(user);
             return authenticationToken;
         } else {
