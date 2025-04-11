6de906a71a67e920450a4065ad62207aa27f9f72
Michael Pointner <mpointner@tgm.ac.at>
2025-04-11 22:49:03 +0200

    Fix SonarQube Issues

--- server/src/main/java/at/ac/tgm/config/WebUntisConfig.java
+++ server/src/main/java/at/ac/tgm/config/WebUntisConfig.java
@@ -14,31 +14,31 @@ import java.io.IOException;
 @Configuration
 public class WebUntisConfig {
     @Value("${webuntis.username}")
-    private String WEBUNITS_USER;
+    private String webunitsUser;
     @Value("${webuntis.password}")
-    private String WEBUNITS_PASSWORD;
+    private String webunitsPassword;
     @Value("${spring.ldap.username}")
-    private String AD_USER;
+    private String activeDirectoryUser;
     @Value("${spring.ldap.password}")
-    private String AD_PASSWORD;
+    private String activeDirectoryPassword;
     
     @Bean
-    public Session login() {
-        String user = WEBUNITS_USER != null && !WEBUNITS_USER.isBlank()
-                ? WEBUNITS_USER
-                : (AD_USER.contains("@") ? AD_USER.split("@")[0] : AD_USER);
-        String password = WEBUNITS_PASSWORD != null && !WEBUNITS_PASSWORD.isBlank()
-                ? WEBUNITS_PASSWORD
-                : AD_PASSWORD;
+    public Session login() throws IOException {
+        String activeDirectoryUsername = activeDirectoryUser != null && activeDirectoryUser.contains("@")
+                ? activeDirectoryUser.split("@")[0]
+                : activeDirectoryUser;
+        String user = webunitsUser != null && !webunitsUser.isBlank()
+                ? webunitsUser
+                : activeDirectoryUsername;
+        String password = webunitsPassword != null && !webunitsPassword.isBlank()
+                ? webunitsPassword
+                : activeDirectoryPassword;
         Session session;
         try {
             session = Session.login(user, password, "https://neilo.webuntis.com", "tgm");
         } catch (LoginException e) {
             log.error(e.getMessage());
             throw new BadCredentialsException("Login to WebUntis failed with your credentials");
-        } catch (IOException e) {
-            log.error(e.getMessage(), e.getCause());
-            throw new RuntimeException(e.getMessage(), e.getCause());
         }
         return session;
     }
