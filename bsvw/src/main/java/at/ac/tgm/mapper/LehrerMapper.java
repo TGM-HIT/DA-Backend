9faf6ecb176a8cdfbcfd895b344ada65236c6e43
Matei <mdragne@student.tgm.ac.at>
2026-03-14 19:17:54 +0100

    - changed prefix on columns in MeldeEntity and RueckgabeEntity - changed Meldeservice to automatic Slf4j without RequiredArgsConstructor and final variable - changed MeldeController to change Bootstickstatus - deleted testimplementation in server

--- bsvw/src/main/java/at/ac/tgm/mapper/LehrerMapper.java
+++ bsvw/src/main/java/at/ac/tgm/mapper/LehrerMapper.java
@@ -3,11 +3,13 @@ package at.ac.tgm.mapper;
 import at.ac.tgm.dto.LehrerDto;
 import at.ac.tgm.entity.LehrerEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LehrerMapper {
 
     LehrerDto toDto(LehrerEntity entity);
 
+    @Mapping(target = "id", ignore = true)
     LehrerEntity toEntity(LehrerDto dto);
 }
